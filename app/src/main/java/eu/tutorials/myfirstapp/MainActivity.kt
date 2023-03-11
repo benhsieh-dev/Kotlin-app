bbff2d7d481fc06865cca2c6082e52eb73df87fa
Ben Hsieh <bqh5026@gmail.com>
2023-03-10 20:46:41 -0600

    Kotlin app

---
+++ app/src/main/java/eu/tutorials/myfirstapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package eu.tutorials.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Kotlin app"
+        }
+    }
+}

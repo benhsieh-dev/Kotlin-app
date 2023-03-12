f2d1034f69c8c00d65931e3dccd71e86274a374d
Ben Hsieh <bqh5026@gmail.com>
2023-03-11 21:20:27 -0600

    Add event listener to text view

--- app/src/main/java/eu/tutorials/myfirstapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/myfirstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package eu.tutorials.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         val btnClickMe = findViewById<Button>(R.id.mybutton)
         btnClickMe.setOnClickListener {
             btnClickMe.text = "Kotlin app"
+        }
+
+        val txtClickMe = findViewById<TextView>(R.id.textView)
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Kotlin app"
+            txtClickMe.text = "Kotlin developer"
         }
     }
 }
